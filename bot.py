2c1156471829fb29ee5b89007a04c597192309ba
Vladislav Moiseev <moiseev.va15@gmail.com>
2025-02-25 14:31:15 +0500

    Зарегистрировал бота в BotFather. Подключен токен. Написаны функции ознакомительные функции start(), hello(), hello_button()

--- bot.py
+++ bot.py
@@ -3,8 +3,32 @@ from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQu
 from gpt import *
 from util import *
 
-# тут будем писать наш код :)
+async def start(update, context):
+    text = load_message('main')
+    await send_photo(update, context, 'main')
+    await send_text(update, context, text)
+
+async def hello(update, context):
+    await send_text(update, context, '*Привет!*')
+    await send_text(update, context, '_Как дела!_')
+    await send_text(update, context, 'Вы написали ' + update.message.text)
+
+    await send_photo(update, context, 'avatar_main')
+    await send_text_buttons(update, context, 'Запустить процесс?', {
+        'start': 'Запустить',
+        'stop': 'Остановить',
+    })
+
+async def hello_button(update, context):
+    query = update.callback_query.data
+    if query == 'start':
+        await send_text(update, context, 'Процесс запущен!')
+    else:
+        await send_text(update, context, 'Процесс остановлен!')
 
 
-app = ApplicationBuilder().token("telegram-token").build()
+app = ApplicationBuilder().token("7675209029:AAH_zwrNV_GdRG1makmf5ilubMA5zP9bc_o").build()
+app.add_handler(CommandHandler('start', start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
