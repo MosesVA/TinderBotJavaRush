9a41a6b001bf281103b04c7c3d14e4b7967f2c4c
Vladislav Moiseev <moiseev.va15@gmail.com>
2025-02-26 23:31:55 +0500

    Добавлены функции gpt_dialog(), date(), date_dialog(), date_button(), message(), message_dialog(), message_button(). Добавлены CommandHandler и CallbackQueryHandler для date и message

--- bot.py
+++ bot.py
@@ -12,18 +12,13 @@ async def start(update, context):
 
     await show_main_menu(update, context, {
         'start': 'главное меню бота',
-        'profile': 'генерация Tinder - профля 😎',
+        'profile': 'генерация Tinder - профиля 😎',
         'opener': 'сообщение для знакомства 🥰',
         'message': 'переписка от вашего имени 😈',
         'date': 'переписка со звездами 🔥',
         'gpt': 'задать вопрос чату GPT 🧠'
     })
 
-async def gpt(update, context):
-    dialog.mode = 'gpt'
-    text = load_message('gpt')
-    await send_photo(update, context, 'gpt')
-    await send_text(update, context, text)
 
 async def gpt_dialog(update, context):
     text = update.message.text
@@ -31,9 +26,79 @@ async def gpt_dialog(update, context):
     answer = await chatgpt.send_question(promt, text)
     await send_text(update, context, answer)
 
+
+async def gpt(update, context):
+    dialog.mode = 'gpt'
+    text = load_message('gpt')
+    await send_photo(update, context, 'gpt')
+    await send_text(update, context, text)
+
+
+async def date(update, context):
+    dialog.mode = 'date'
+    text = load_message('date')
+    await send_photo(update, context, 'date')
+    await send_text_buttons(update, context, text, {
+        'date_grande': 'Ариана Гранде',
+        'date_robbie': 'Марго Робби',
+        'date_zendaya': 'Зендея',
+        'date_gosling': 'Райан Гослинг',
+        'date_hardy': 'Том Харди'
+    })
+
+
+async def date_dialog(update, context):
+    text = update.message.text
+    my_message = await send_text(update, context, 'Набирает текст...')
+    answer = await chatgpt.add_message(text)
+    await my_message.edit_text(answer)
+
+
+async def date_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+
+    await send_photo(update, context, query)
+    await send_text(update, context, "Отличный выбор! Пригласите девушку(парня) на свидание за 5 сообщений.")
+
+    promt = load_prompt(query)
+    chatgpt.set_prompt(promt)
+
+
+async def message(update, context):
+    dialog.mode = 'message'
+    text = load_message('message')
+    await send_photo(update, context, 'message')
+    await send_text_buttons(update, context, text, {
+        'message_next': 'Следующее сообщение',
+        'message_date': 'Пригласить на свидание'
+    })
+    dialog.list.clear()
+
+
+async def message_dialog(update, context):
+    text = update.message.text
+    dialog.list.append(text)
+
+
+async def message_button(update, context):
+    query = update.callback_query.data
+    await update.callback_query.answer()
+
+    promt = load_prompt(query)
+    user_chat_history = '\n\n'.join(dialog.list)
+    my_message = await send_text(update, context, 'ChatGPT думает над вариантами ответов...')
+    answer = await chatgpt.send_question(promt, user_chat_history)
+    await my_message.edit_text(answer)
+
+
 async def hello(update, context):
     if dialog.mode == 'gpt':
         await gpt_dialog(update, context)
+    elif dialog.mode == 'date':
+        await date_dialog(update, context)
+    elif dialog.mode == 'message':
+        await message_dialog(update, context)
     else:
         await send_text(update, context, '*Привет!*')
         await send_text(update, context, '_Как дела!_')
@@ -45,6 +110,7 @@ async def hello(update, context):
             'stop': 'Остановить',
         })
 
+
 async def hello_button(update, context):
     query = update.callback_query.data
     if query == 'start':
@@ -52,8 +118,10 @@ async def hello_button(update, context):
     else:
         await send_text(update, context, 'Процесс остановлен!')
 
+
 dialog = Dialog()
 dialog.mode = None
+dialog.list = []
 
 chatgpt = ChatGptService(
     token='gpt:A02TVYVCwCQQ_OH2tdCyUd2HKqN4GP0xaMtAUWF72T9BdIro0-uIwKS8orBFCeYf_aOW-SBy7xJFkblB3Tz3TILu-3bNVFZqtDZtL5dguxjkKTQ1UO3rumIlFjgYt00HL4xUpvo1IqH_9HI69tGsbgnevKkS')
@@ -61,6 +129,12 @@ chatgpt = ChatGptService(
 app = ApplicationBuilder().token("7675209029:AAH_zwrNV_GdRG1makmf5ilubMA5zP9bc_o").build()
 app.add_handler(CommandHandler('start', start))
 app.add_handler(CommandHandler('gpt', gpt))
+app.add_handler(CommandHandler('date', date))
+app.add_handler(CommandHandler('message', message))
+
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, hello))
+
+app.add_handler(CallbackQueryHandler(date_button, pattern='^date_.*'))
+app.add_handler(CallbackQueryHandler(message_button, pattern='^message_.*'))
 app.add_handler(CallbackQueryHandler(hello_button))
 app.run_polling()
